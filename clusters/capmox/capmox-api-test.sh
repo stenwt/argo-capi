b6d971714f8ab0306d9d93a7c3b71de31ea2dcdf
Sten Turpin <web@sud0.com>
2025-11-16 16:41:32 -0600

    trying key names used by kamaji

--- clusters/capmox/capmox-api-test.sh
+++ clusters/capmox/capmox-api-test.sh
@@ -3,6 +3,14 @@ URL=$(kubectl get secret proxmox-credentials -n capmox -o jsonpath='{.data.url}'
 TOKEN_ID=$(kubectl get secret proxmox-credentials -n capmox -o jsonpath='{.data.token-id}' | base64 -d)
 TOKEN_SECRET=$(kubectl get secret proxmox-credentials -n capmox -o jsonpath='{.data.token-secret}' | base64 -d)
 
-# Test the API (use -k to skip TLS verification for testing)
+# Test the API version
 curl -k "${URL}/api2/json/version" \
+  -H "Authorization: PVEAPIToken=${TOKEN_ID}=${TOKEN_SECRET}"
+
+# Test the API nodes ep
+curl -k "${URL}/api2/json/nodes" \
+  -H "Authorization: PVEAPIToken=${TOKEN_ID}=${TOKEN_SECRET}"
+
+# Test the API cluster resources ep
+curl -k "${URL}/api2/json/cluster/resources" \
   -H "Authorization: PVEAPIToken=${TOKEN_ID}=${TOKEN_SECRET}"
